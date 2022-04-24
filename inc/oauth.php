ecd48cf9551584b5548f87f4d1cb86207fb6a361
Emmanuel Campait <ecampait@gmail.com>
2022-04-24 11:41:45 +0200

    minor change

--- inc/oauth.php
+++ inc/oauth.php
@@ -1,22 +1,18 @@
 <?php
-/* oauth.php Azure AD oAuth class
- *
- * Katy Nicholson, last updated 17/11/2021
- *
- * https://github.com/CoasterKaty
- * https://katytech.blog/
- * https://twitter.com/coaster_katy
+/**
+ * 
+ * Original project: Katy Nicholson - https://github.com/CoasterKaty
  *
  */
 
 class modOAuth
 {
-	function errorMessage($message)
-	{
-		return $message;
-	}
-
-	function generateRequest($data)
+	/**
+	 * xxxxx xxxxx xxxxx
+	 *
+	 * @return xxxxx
+	 */
+	public function generateRequest($data)
 	{
 		if (_OAUTH_METHOD == 'certificate')
 		{
@@ -35,14 +31,14 @@ class modOAuth
 				'aud' => 'https://login.microsoftonline.com/' . _OAUTH_TENANTID . '/v2.0',
 				'exp' => date('U', strtotime('+10 minute')),
 				'iss' => _OAUTH_CLIENTID,
-				'jti' => $this->uuid(),
+				'jti' => $this->_uuid(),
 				'nbf' => date('U'),
 				'sub' => _OAUTH_CLIENTID)
 			);
 			$caSignature = '';
-			$caData = $this->base64UrlEncode($caHeader) . '.' . $this->base64UrlEncode($caPayload);
+			$caData = $this->_base64UrlEncode($caHeader) . '.' . $this->_base64UrlEncode($caPayload);
 			openssl_sign($caData, $caSignature, $certKey, OPENSSL_ALGO_SHA256);
-			$caSignature = $this->base64UrlEncode($caSignature);
+			$caSignature = $this->_base64UrlEncode($caSignature);
 			$clientAssertion = $caData . '.' . $caSignature;
 	
 			return $data . '&client_assertion=' . $clientAssertion . '&client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer';
@@ -54,7 +50,12 @@ class modOAuth
 		}
 	}
 
-	function postRequest($endpoint, $data)
+	/**
+	 * xxxxx xxxxx xxxxx
+	 *
+	 * @return xxxxx
+	 */
+	public function postRequest($endpoint, $data)
 	{
 		$ch = curl_init('https://login.microsoftonline.com/' . _OAUTH_TENANTID . '/oauth2/v2.0/' . $endpoint);
 
@@ -70,7 +71,7 @@ class modOAuth
 
 		if ($cError = curl_error($ch))
 		{
-			echo $this->errorMessage($cError);
+			echo $this->_errorMessage($cError);
 			exit;
 		}
 
@@ -79,30 +80,49 @@ class modOAuth
 		return $response;
 	}
 
-	function base64UrlEncode($toEncode)
+	/**
+	 * xxxxx xxxxx xxxxx
+	 *
+	 * @return xxxxx
+	 */
+	protected function _base64UrlEncode($toEncode)
 	{
 		return str_replace('=', '', strtr(base64_encode($toEncode), '+/', '-_'));
 	}
 
-	function uuid()
+	/**
+	 * uuid function is not my code, but unsure who the original author is. KN
+	 * uuid version 4
+	 *
+	 * @return xxxxx
+	 */
+	protected function _uuid()
 	{
-		// uuid function is not my code, but unsure who the original author is. KN
-		// uuid version 4
-		return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+		return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
 			// 32 bits for "time_low"
-			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+			mt_rand(0, 0xffff), mt_rand(0, 0xffff),
 			// 16 bits for "time_mid"
-			mt_rand( 0, 0xffff ),
+			mt_rand(0, 0xffff),
 			// 16 bits for "time_hi_and_version",
 			// four most significant bits holds version number 4
-			mt_rand( 0, 0x0fff ) | 0x4000,
+			mt_rand(0, 0x0fff) | 0x4000,
 			// 16 bits, 8 bits for "clk_seq_hi_res",
 			// 8 bits for "clk_seq_low",
 			// two most significant bits holds zero and one for variant DCE1.1
-			mt_rand( 0, 0x3fff ) | 0x8000,
+			mt_rand(0, 0x3fff) | 0x8000,
 			// 48 bits for "node"
-			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+			mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
 		);
+	}
+
+	/**
+	 * xxxxx xxxxx xxxxx
+	 *
+	 * @return xxxxx
+	 */
+	protected function _errorMessage($message): string
+	{
+		return $message;
 	}
 }
 
